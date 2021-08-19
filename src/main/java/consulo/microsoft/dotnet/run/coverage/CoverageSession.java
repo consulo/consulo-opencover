13df866bbee89b00a7a7ea9e617620c30a234f1a
VISTALL <vistall.valeriy@gmail.com>
2021-08-19 10:39:12 +0300

    jakarta fix (to prev)

--- src/main/java/consulo/microsoft/dotnet/run/coverage/CoverageSession.java
+++ src/main/java/consulo/microsoft/dotnet/run/coverage/CoverageSession.java
@@ -16,9 +16,9 @@
 
 package consulo.microsoft.dotnet.run.coverage;
 
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 /**
  * @author VISTALL
